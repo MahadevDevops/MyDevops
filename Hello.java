bc4a8ccc24191319079d9358428ab91e839d2a2b
MahadevDevops <34904413+MahadevDevops@users.noreply.github.com>
2018-01-02 17:38:26 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,12 @@
 clas  HelloWorld() {
 public static void main(){
-
-
-
-system.out.println("This is my first HelloWorld")/n
+  system.out.println("This is my first HelloWorld")
+  for(i=0;i<10;i++){
+    i=j=1
+    system.out.println("value of i is",i)  
+    
+  }
+    
+  
 }
 } 
